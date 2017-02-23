432330a4207119cd98c2feba2a4c1ed33d3182da
KoenJJS <k.smeets@hotmail.nl>
2017-02-23 13:59:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,52 +38,6 @@ var testing = location.hostname===''||location.hostname==='localhost',
 	        'Bij de Internationale Junior Science Olympiad eindigde ik in de nationale eindronde op de 9e plek.',
 	      ]
 	    },
-	    {
-	      title:'TTA',
-	      link:['https://larsgw.github.io/InlinEdit/','https://github.com/larsgw/inlinedit/'],
-	      img:'img/slider_img-06.png',
-	      text:[
-		'Small plugin for editing markdown-like rich text. Not perfect. Will be used for Webapp Presentations.',
-		'<a href="https://github.com/larsgw/InlinEdit">Repository</a><br>'+
-		'<a href="https://larsgw.github.io/InlinEdit/">Demo</a>'
-	      ]
-	    },
-	    {
-	      title:'karinmajoor.nl',
-	      link:['http://karinmajoor.nl/'],
-	      img:'img/slider_img-03.png',
-	      text:[
-		'A small web page with AngularJS.',
-		'<a href="http://karinmajoor.nl">Link</a>'
-	      ]
-	    },
-	    {
-	      title:'Webapp Presentations',
-	      img:'img/slider_img-05.png',
-	      text:[
-		'Work in progress. A webapp/website where you can make your own Prezi-like presentations and interactive slides. Together with Robin van Merle (<a href="https://twitter.com/RobinvanMerle">@RobinvanMerle</a>).',
-		'<a href="https://goo.gl/photos/PsGiqVArZTsdMng27">Screenshots</a><br>'+
-		'<a href="https://jsfiddle.net/LarsGW/9eu5pmud/3/">3D Demo</a>'
-	      ]
-	    },
-	    {
-	      title:'egonw.github.io',
-	      link:['http://egonw.github.io/','https://github.com/egonw/egonw.github.com/'],
-	      img:'img/slider_img-01.png',
-	      text:[
-		'Some design improvements on the github.io page of Egon Willighagen',
-		'<a href="http://egonw.github.io/">Link</a>'
-	      ]
-	    },
-	    {
-	      title:'Bounce',
-	      link:['http://eckartcollege.nl/bounce/Bounce.html'],
-	      img:'img/slider_img-04.png',
-	      text:[
-		'A small web game with just HTML, CSS, JavaScript and jQuery.',
-		'<a href="http://eckartcollege.nl/bounce/Bounce.html">Demo</a>'
-	      ]
-	    }
 	  ]
 	}
       };
